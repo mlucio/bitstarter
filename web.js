2e68b4016a82f66abac1663976d4a0874a5d08ab
Marco Lucio <mlucio@gmail.com>
2013-07-05 04:55:42 +0000

    Modified web.js 6

--- web.js
+++ web.js
@@ -3,13 +3,9 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var linea = function(){
-return fs.readFileSync('index.html').toString;
-}
-
 
 app.get('/', function(request, response) {
-  response.send(linea());
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
